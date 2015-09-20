d2fb1f7e035378186caf1ecfea7a6d3b00cf9ba7
udaymitra <udaymitra4u@gmail.com>
2015-09-20 16:07:42 -0700

    added search functionality

--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -10,17 +10,19 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITabBarDelegate {
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITabBarDelegate, UISearchBarDelegate {
     
     let BOX_OFFICE_URL = "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json"
     let TOP_DVD_URL = "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json"
 
     @IBOutlet weak var moviesTableView: UITableView!
     var moviesDictionaryArray:[NSDictionary]?
-    var uiRefreshControl : UIRefreshControl!    
+    var filteredMoviesDictionaryArray:[NSDictionary]?
+    var uiRefreshControl : UIRefreshControl!
     @IBOutlet weak var errorView: UIView!
     @IBOutlet weak var errorViewLabel: UILabel!
     @IBOutlet weak var movieDvdSelectionTabBar: UITabBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         moviesTableView.dataSource = self
         
         movieDvdSelectionTabBar.delegate = self
+        searchBar.delegate = self
         
         // add refresh control
         uiRefreshControl = UIRefreshControl()
@@ -54,12 +57,12 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesDictionaryArray?.count ?? 0
+        return filteredMoviesDictionaryArray?.count ?? 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let movieCell = moviesTableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MoviesTableViewCell
-        let thisMovieDict = self.moviesDictionaryArray![indexPath.row]
+        let thisMovieDict = self.filteredMoviesDictionaryArray![indexPath.row]
         let movieHelper = MovieHelper(movie: thisMovieDict)
         movieCell.movieLabelName.text = movieHelper.getTitle()
         movieCell.synopsisLabel.text = movieHelper.getSynopsis()
@@ -96,6 +99,9 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 if let allData = object["movies"] {
                     self.moviesDictionaryArray = allData as? [NSDictionary]
                     
+                    // copy to filteredMoviedDictionaryArray
+                    self.filteredMoviesDictionaryArray = self.moviesDictionaryArray!
+                    
                     // reload view with new data
                     self.moviesTableView.reloadData()
                     
@@ -123,9 +129,43 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let cell = sender as! UITableViewCell
         let indexPath = moviesTableView.indexPathForCell(cell)
-        let movie = moviesDictionaryArray![indexPath!.row]
+        let movie = filteredMoviesDictionaryArray![indexPath!.row]
         let movieDetailViewController = segue.destinationViewController as! MovieDetaiViewController
         movieDetailViewController.movie = movie
     }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+
+        filteredMoviesDictionaryArray = self.moviesDictionaryArray
+        moviesTableView.reloadData()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText == "") {
+            self.filteredMoviesDictionaryArray = self.moviesDictionaryArray
+        } else {
+            filteredMoviesDictionaryArray = moviesDictionaryArray!.filter({ (movieDict : NSDictionary) -> Bool in
+                let movieHelper = MovieHelper(movie: movieDict)
+                return matchesMovieName(movieHelper, searchString: searchText)
+            })
+        }
+        
+        // reload view with new data
+        self.moviesTableView.reloadData()
+    }
+    
+    func matchesMovieName(movieHelper: MovieHelper, searchString: String) -> Bool {
+        let range = movieHelper.getTitle()!.rangeOfString(searchString, options: NSStringCompareOptions.CaseInsensitiveSearch)
+        return range != nil
+    }
+    
+    
     
 }
